d09c6277d71bbe0570e455ec04685b68961aa058
Wren Powell <wrenp@duck.com>
2024-12-21 11:12:18 -0500

    Update deprecated Terraform resource fields

--- infra/dns.tf
+++ infra/dns.tf
@@ -2,7 +2,7 @@ resource "cloudflare_record" "apex_cname" {
   zone_id = data.cloudflare_zone.smorgasbord.id
   type    = "CNAME"
   name    = "@"
-  value   = cloudflare_pages_project.smorgasbord.subdomain
+  content = cloudflare_pages_project.smorgasbord.subdomain
   proxied = true
 }
 
@@ -18,18 +18,18 @@ resource "cloudflare_record" "apex_txt_sl_verification" {
   zone_id = data.cloudflare_zone.smorgasbord.id
   type    = "TXT"
   name    = "@"
-  value   = "sl-verification=bbozlsqmqlrnuufglpzzbqmuruhwvn"
+  content = "sl-verification=bbozlsqmqlrnuufglpzzbqmuruhwvn"
   proxied = false
 }
 
 resource "cloudflare_record" "apex_mx" {
   for_each = {
     route1 = {
-      value    = "mx1.simplelogin.co."
+      content  = "mx1.simplelogin.co."
       priority = 10
     }
     route2 = {
-      value    = "mx2.simplelogin.co."
+      content  = "mx2.simplelogin.co."
       priority = 20
     }
   }
@@ -37,7 +37,7 @@ resource "cloudflare_record" "apex_mx" {
   zone_id  = data.cloudflare_zone.smorgasbord.id
   type     = "MX"
   name     = "@"
-  value    = each.value.value
+  content  = each.value.content
   priority = each.value.priority
   proxied  = false
 }
@@ -46,32 +46,32 @@ resource "cloudflare_record" "apex_txt_spf" {
   zone_id = data.cloudflare_zone.smorgasbord.id
   type    = "TXT"
   name    = "@"
-  value   = "v=spf1 include:simplelogin.co ~all"
+  content = "v=spf1 include:simplelogin.co ~all"
   proxied = false
 }
 
 resource "cloudflare_record" "apex_cname_dkim" {
   for_each = {
     record1 = {
-      name  = "dkim._domainkey"
-      value = "dkim._domainkey.simplelogin.co."
+      name    = "dkim._domainkey"
+      content = "dkim._domainkey.simplelogin.co."
     }
 
     record2 = {
-      name  = "dkim02._domainkey"
-      value = "dkim02._domainkey.simplelogin.co."
+      name    = "dkim02._domainkey"
+      content = "dkim02._domainkey.simplelogin.co."
     }
 
     record3 = {
-      name  = "dkim03._domainkey"
-      value = "dkim03._domainkey.simplelogin.co."
+      name    = "dkim03._domainkey"
+      content = "dkim03._domainkey.simplelogin.co."
     }
   }
 
   zone_id = data.cloudflare_zone.smorgasbord.id
   type    = "CNAME"
   name    = each.value.name
-  value   = each.value.value
+  content = each.value.content
   proxied = false
 }
 
@@ -79,7 +79,7 @@ resource "cloudflare_record" "apex_txt_dmarc" {
   zone_id = data.cloudflare_zone.smorgasbord.id
   type    = "TXT"
   name    = "_dmarc"
-  value   = "v=DMARC1; p=quarantine; pct=100; adkim=s; aspf=s"
+  content = "v=DMARC1; p=quarantine; pct=100; adkim=s; aspf=s"
   proxied = false
 }
 
@@ -87,6 +87,6 @@ resource "cloudflare_record" "umami_cname" {
   zone_id = data.cloudflare_zone.smorgasbord.id
   type    = "CNAME"
   name    = "umami"
-  value   = "strict-elk.pikapod.net"
+  content = "strict-elk.pikapod.net"
   proxied = false
 }
